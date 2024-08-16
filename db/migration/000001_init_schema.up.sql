b219580fccfb1d4b685437f22dd22ce9b15965d2
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-08-16 18:58:56 +0330

    Update 000001_init_schema.up.sql

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE "accounts" (
 
 CREATE TABLE "entries" (
                            "id" bigserial PRIMARY KEY,
-                           "account_id" bigint,
+                           "account_id" bigint NOT NULL ,
                            "amount" bigint NOT NULL,
                            "created_at" timestamptz DEFAULT now(),
                            CONSTRAINT fk_account FOREIGN KEY ("account_id") REFERENCES "accounts" ("id")
