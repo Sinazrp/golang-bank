b169fcf9e038b316df8585768a82a210ec3999ee
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-08-17 21:22:13 +0330

    Update main_test.go

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,14 +10,16 @@ import (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open("postgres", "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable")
+	var err error
+	testDB, err = sql.Open("postgres", "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable")
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	m.Run()
 }
