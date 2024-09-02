5aab6ab35f6bb1870a5b5a41a95406476adfd086
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-09-02 19:10:29 +0330

    update main to run server and connetc to db

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/sinazrp/golang-bank/api"
+	db "github.com/sinazrp/golang-bank/db/sqlc"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	log.Printf("Hello, World!")
+
+	connection, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db.NewStore(connection)
+	server := api.NewServer(store)
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 
 }
