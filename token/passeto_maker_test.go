53d737026373f763567733cbb45f0b7f3dcbcbb2
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-10-11 23:10:58 +0330

    create create Token test

---
+++ token/passeto_maker_test.go
@@ -0,0 +1,30 @@
+package token
+
+import (
+	"github.com/sinazrp/golang-bank/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestNewPasetoMaker(t *testing.T) {
+
+	maker, err := NewPasetoMaker(util.RandomString(32))
+
+	require.NoError(t, err)
+	username := util.RandomOwner()
+	duration := time.Minute
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+
+}
