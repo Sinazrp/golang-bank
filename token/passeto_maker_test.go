5e415f2056207c8a7f2e61d07863e76c0cc9c287
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-10-11 23:38:19 +0330

    implement expired and invalid token test

--- token/passeto_maker_test.go
+++ token/passeto_maker_test.go
@@ -28,3 +28,29 @@ func TestNewPasetoMaker(t *testing.T) {
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
 
 }
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+	username := util.RandomOwner()
+	token, err := maker.CreateToken(username, -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+
+}
+func TestInvalidPassetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+	token, err := maker.CreateToken(util.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	payload, err := maker.VerifyToken(token + "extra")
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+
+}
