ffff0588c5ea3da0e5689a7fab1384a2871b1558
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-10-12 00:41:24 +0330

    Update main_test.go

--- api/main_test.go
+++ api/main_test.go
@@ -2,10 +2,26 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	db "github.com/sinazrp/golang-bank/db/sqlc"
+	"github.com/sinazrp/golang-bank/util"
+	"log"
 	"testing"
+	"time"
 
 	_ "github.com/lib/pq"
 )
+
+func NewTestServer(store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+	server, err := NewServer(store, config)
+	if err != nil {
+		log.Fatal("cannot create server")
+	}
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
