edd647eb1414f9880a32d425b2c65b8ec152d461
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-10-12 00:41:17 +0330

    Update server.go

--- api/server.go
+++ api/server.go
@@ -1,22 +1,33 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/sinazrp/golang-bank/db/sqlc"
+	"github.com/sinazrp/golang-bank/token"
+	"github.com/sinazrp/golang-bank/util"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	router     *gin.Engine
+	tokenMaker token.Maker
 }
 
 // NewServer creates a new HTTP server and setup routing for the account
 // resource.
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(store db.Store, config util.Config) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker : %w", err)
+
+	}
+
+	server := &Server{store: store, tokenMaker: tokenMaker}
+
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 		v.RegisterValidation("amount", validAmount)
@@ -24,28 +35,8 @@ func NewServer(store db.Store) *Server {
 		v.RegisterValidation("password", validPassword)
 	}
 
-	// Create account
-	router.POST("/accounts", server.createAccount)
-
-	// Get account
-	router.GET("/accounts/:id", server.getAccount)
-
-	// List accounts
-	router.GET("/accounts", server.getAccountList)
-
-	// Delete account
-	router.DELETE("/accounts/:id", server.deleteAccount)
-
-	// Update account
-	router.PUT("/accounts", server.updateAccount)
-
-	// Create Transfer
-	router.POST("/transfers", server.createTransfer)
-
-	//create User
-	router.POST("/user", server.createUser)
-	server.router = router
-	return server
+	server.setupRoutes()
+	return server, nil
 }
 
 // Start runs the HTTP server on a specific address.
