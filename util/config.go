80286c32d26781641bec3b0f14f38d0317799910
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-10-18 18:21:26 +0330

    Update config.go

--- util/config.go
+++ util/config.go
@@ -1,50 +1,52 @@
 package util
 
 import (
-	"github.com/spf13/viper"
 	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
 type Config struct {
-	DBDriver            string        `mapstructure:"DB_DRIVER"`
-	DBSource            string        `mapstructure:"DB_SOURCE"`
-	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
-	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
-	DatabaseURL         string        `mapstructure:"DATABASE_URL"`
+	DBDriver            string
+	DBSource            string
+	ServerAddress       string
+	TokenSymmetricKey   string
+	AccessTokenDuration time.Duration
 }
 
-func LoadConfig(path string) (config Config, err error) {
-	//viper.AddConfigPath(path)
-	//viper.SetConfigType("env")
+func LoadConfig() (config Config, err error) {
+	config.DBSource = os.Getenv("DB_SOURCE")
+	if config.DBSource == "" {
+		log.Fatal("DB_SOURCE environment variable is not set")
+	}
 
-	viper.AutomaticEnv()
-	//err = viper.ReadInConfig()
-	viper.GetString("DB_DRIVER")
-	viper.GetString("DB_SOURCE")
-	viper.GetString("SERVER_ADDRESS")
-	viper.GetString("TOKEN_SYMMETRIC_KEY")
-	viper.GetTime("ACCESS_TOKEN_DURATION")
-	viper.GetString("DATABASE_URL")
-	//
-	//if dbDriver == "" || dbSource == "" || serverAddress == "" || tokenSymmetricKey == "" || accessTokenDuration == "" || databaseURL == "" {
-	//	log.Fatal("One or more required environment variables are not set")
-	//}
-	//
-	//if err != nil {
-	//	return
-	//}
-	//err = viper.Unmarshal(&config)
+	config.DBDriver = os.Getenv("DB_DRIVER")
+	if config.DBDriver == "" {
+		log.Fatal("DB_DRIVER environment variable is not set")
+	}
 
-	err = viper.Unmarshal(&config)
+	config.ServerAddress = os.Getenv("SERVER_ADDRESS")
+	if config.ServerAddress == "" {
+		log.Fatal("SERVER_ADDRESS environment variable is not set")
+	}
+
+	config.TokenSymmetricKey = os.Getenv("TOKEN_SYMMETRIC_KEY")
+	if config.TokenSymmetricKey == "" {
+		log.Fatal("TOKEN_SYMMETRIC_KEY environment variable is not set")
+	}
+
+	accessTokenDurationStr := os.Getenv("ACCESS_TOKEN_DURATION")
+	if accessTokenDurationStr == "" {
+		log.Fatal("ACCESS_TOKEN_DURATION environment variable is not set")
+	}
+
+	accessTokenDuration, err := strconv.ParseInt(accessTokenDurationStr, 10, 64)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Invalid ACCESS_TOKEN_DURATION value")
 	}
 
-	if config.DBDriver == "" || config.DBSource == "" || config.ServerAddress == "" || config.TokenSymmetricKey == "" || config.DatabaseURL == "" || config.AccessTokenDuration == 0 {
-		log.Fatal("One or more required environment variables are not set")
-	}
+	config.AccessTokenDuration = time.Duration(accessTokenDuration) * time.Second
+
 	return
-
 }
