3c2f7086833d133af9e75c4ad35bdf0208a51914
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-10-18 18:05:31 +0330

    change config setting

--- util/config.go
+++ util/config.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/spf13/viper"
+	"log"
 	"time"
 )
 
@@ -11,18 +12,40 @@ type Config struct {
 	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
 	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	DatabaseURL         string        `mapstructure:"DATABASE_URL"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigType("env")
+	//viper.AddConfigPath(path)
+	//viper.SetConfigType("env")
 
 	viper.AutomaticEnv()
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
-	}
+	//err = viper.ReadInConfig()
+	//dbDriver := viper.GetString("DB_DRIVER")
+	//dbSource := viper.GetString("DB_SOURCE")
+	//serverAddress := viper.GetString("SERVER_ADDRESS")
+	//tokenSymmetricKey := viper.GetString("TOKEN_SYMMETRIC_KEY")
+	//accessTokenDuration := viper.GetString("ACCESS_TOKEN_DURATION")
+	//databaseURL := viper.GetString("DATABASE_URL")
+	//
+	//if dbDriver == "" || dbSource == "" || serverAddress == "" || tokenSymmetricKey == "" || accessTokenDuration == "" || databaseURL == "" {
+	//	log.Fatal("One or more required environment variables are not set")
+	//}
+	//
+	//if err != nil {
+	//	return
+	//}
+	//err = viper.Unmarshal(&config)
+	viper.AutomaticEnv()
+
 	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if config.DBDriver == "" || config.DBSource == "" || config.ServerAddress == "" || config.TokenSymmetricKey == "" || config.DatabaseURL == "" || config.AccessTokenDuration == 0 {
+		log.Fatal("One or more required environment variables are not set")
+	}
 	return
 
 }
